ab50532b54e90c3b7534b661e313ddc415d40faf
M Arbo <marbo.phd@gmail.com>
2021-12-04 19:29:23 -0600

    Puzzle 04

---
+++ 04/hard-solution.py
@@ -0,0 +1,57 @@
+import os
+
+class BingoBoard:
+    def __init__(self, lines) -> None:
+        self.board = []
+        self.called = []
+        for myRow in lines:
+            self.board.append(myRow);
+            self.called.append([False] * len(myRow))
+    
+    def call(self, number):
+        for i in range(5):
+            for j in range(5):
+                if self.board[i][j] == number:
+                   self.called[i][j] = True;
+                   break
+        return None
+    
+    def win(self):
+        for i in range(5):
+            if(all(self.called[i])):
+                return True
+        for j in range(5):
+            if(all([self.called[i][j] for i in range(5)])):
+                return True
+        return False
+    
+    def score(self):
+        value = 0
+        for i in range(5):
+            for j in range(5):
+                if(not self.called[i][j]):
+                    value += self.board[i][j]
+        return value
+
+
+lines = open(os.path.join(os.path.dirname(__file__),"input")).readlines()
+numbers = [int(number) for number in lines[0].split(",")]
+
+lines = lines[2:len(lines)]
+boards = []
+board = []
+for line in lines:
+    if line.strip() == "":
+        boards.append(BingoBoard(board))
+        board = []
+    else:
+        board.append( [int(number) for number in line.split()] )
+
+for number in numbers:
+    if len(boards) > 1:
+        boards = [board for board in boards if not board.win()]
+    for board in boards:
+        board.call(number)
+    if len(boards) == 1 and board.win():
+        print(board.score() * number)
+        break
